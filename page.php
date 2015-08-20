0338e9b2641651e5d7363acde0553e5e26bb24f0
OanaRaluca <oana.petre14@yahoo.com>
2015-08-20 15:52:37 +0300

    Create page.php

---
+++ page.php
@@ -0,0 +1,27 @@
+<?php
+defined('ABSPATH') or die("please don't runs scripts");
+
+get_header(); ?>
+  
+<div class="wrap">
+	<div class="container">
+		<div class="col-md-8 blog">
+			
+			<?php if ( have_posts() ) :
+			
+				while ( have_posts() ) : the_post(); 
+				
+				get_template_part( 'content', 'page');
+				
+				endwhile;
+					
+				else :
+			
+				get_template_part( 'content', 'none');
+			
+				endif; ?>
+					
+			</div><!-- /col-md-8 blog -->
+		<?php get_sidebar(); ?>
+		</div><!-- /container -->
+<?php get_footer(); ?>
